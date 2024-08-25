31924b2c80f5cdadd94b9ef9e1e9667486d8a187
Vince Dority <vncdority@gmail.com>
2024-08-25 14:20:24 -0400

    First commit: Tailwind theme added for light and dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,54 @@
 import type { Config } from "tailwindcss";
+import {createThemes} from "tw-colors";
+import colors from "tailwindcss/colors";
 
+//**  Light/Dark theme setup -start**//
+// create baseColor scheme<array>
+  const baseColor =[
+    "gray",
+    "red",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "purple",
+    "pink",
+  ];
+  // create color mapping
+  /**
+   * Used to create light/dark theme
+   * 
+   */
+  const shadeMapping ={
+    "50":"900",
+    "100":"800",
+    "200":"700",
+    "300":"600",
+    "400":"500",
+    "500":"400",
+    "600":"300",
+    "700":"200",
+    "800":"100",
+    "900":"50",
+  };
+  /**
+   * generateThemeObject()
+   */
+const generateThemeObject = (colors:any, mapping:any, invert = false) =>{
+  const theme: any = {};
+
+  baseColor.forEach((color)=>{
+  theme[color] = {};
+  Object.entries(mapping).forEach(([key,value]:any)=>{
+    const shadeKey = invert ? value: key;
+    theme[color][key] = colors[color][shadeKey];
+  });
+  });
+  return theme;
+}
+// lightTheme
+const lightTheme = generateThemeObject(colors,shadeMapping);
+const darkTheme = generateThemeObject(colors,shadeMapping,true);
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
